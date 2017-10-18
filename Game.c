bde3943a3dd269dee28746f680c2f7ee40471e4b
PChenA <32757673+PChenA@users.noreply.github.com>
2017-10-18 12:24:31 +1100

    Update Game.c
    I attempted to write the code for int NumOfSuit on line 585.

--- Game.c
+++ Game.c
@@ -583,9 +583,21 @@ int numCards
 // Get the number of cards in the initial deck of a particular
 // suit.
 int numOfSuit
-(Game game, suit suit);
-
-
+(Game game, suit suit)
+{
+    // made a while loop to check each node value if they correspond to the suit,
+    int i = 0;
+    int count = 0;
+    // assuming cards_in_game is an array
+    while (i < deckSize)) {
+        if ((*game).cards_in_game[i] == suit) {
+                count++
+        }
+        i++;
+    }
+        
+    return count;
+}
 
 // Get the number of cards in the initial deck of a particular color.
 int numOfColor
